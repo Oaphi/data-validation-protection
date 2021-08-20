c049f5c74d0167f63fee1aee98acfbe9e919693c
Oleg Valter <o.a.philippov@gmail.com>
2021-08-20 17:01:33 +0300

    added unit tests (and related utils)

---
+++ test/protection.spec.ts
@@ -0,0 +1,110 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+import {
+    parseJsonConfigFileContent,
+    readConfigFile,
+    sys,
+    transpile,
+} from "typescript";
+import { runInThisContext } from "vm";
+
+const configContent = readConfigFile("./tsconfig.json", sys.readFile);
+const tsconfig = parseJsonConfigFileContent(configContent.config, sys, "./");
+
+const protectionContent = sys.readFile("./src/protection.ts")!;
+const protection = transpile(protectionContent, tsconfig.options);
+
+runInThisContext(protection + "\n\nObject.assign(this, { Protection });");
+
+type P = typeof Protection;
+
+declare namespace globalThis {
+    const Protection: P;
+}
+
+describe("Protection", () => {
+    const bareProtection = {
+        _users: [],
+        addEditor(
+            this: { _users: Partial<GoogleAppsScript.Base.User>[] },
+            tgt: string | GoogleAppsScript.Base.User
+        ) {
+            const newUser =
+                typeof tgt === "string"
+                    ? {
+                          getEmail() {
+                              return tgt;
+                          },
+                      }
+                    : tgt;
+
+            this._users.push(newUser);
+            return this;
+        },
+        removeEditor(
+            this: { _users: GoogleAppsScript.Base.User[] },
+            tgt: string | GoogleAppsScript.Base.User
+        ) {
+            const email = typeof tgt === "string" ? tgt : tgt.getEmail();
+            this._users.splice(
+                this._users.findIndex((u) => u.getEmail() === email),
+                1
+            );
+            return this;
+        },
+        removeEditors(
+            this: { _users: GoogleAppsScript.Base.User[] },
+            tgts: (string | GoogleAppsScript.Base.User)[]
+        ) {
+            const emails = tgts.map((u) =>
+                typeof u === "string" ? u : u.getEmail()
+            );
+
+            this._users = this._users.filter(
+                (u: GoogleAppsScript.Base.User) =>
+                    !emails.includes(u.getEmail())
+            );
+            return this;
+        },
+        getEditors(this: { _users: GoogleAppsScript.Base.User[] }) {
+            return [...this._users];
+        },
+    } as unknown as GoogleAppsScript.Spreadsheet.Protection;
+
+    const protection = new globalThis.Protection(bareProtection);
+
+    const sandbox = sinon.createSandbox();
+
+    beforeEach(() => sandbox.spy(bareProtection));
+    afterEach(() => {
+        sandbox.restore();
+        bareProtection.removeEditors(bareProtection.getEditors());
+    });
+
+    it("should correctly attempt to add an editor", () => {
+        const editor = "jane@doe.com";
+        protection.addEditor(editor);
+
+        const emails = bareProtection.getEditors().map((u) => u.getEmail());
+
+        expect(emails).to.have.length(1);
+        expect(emails).to.contain(editor);
+    });
+
+    it("should correctly attempt to remove an editor", () => {
+        const editor = "john@doe.org";
+        protection.addEditor(editor);
+        protection.removeEditor(editor);
+
+        expect(bareProtection.getEditors()).to.be.empty;
+    });
+
+    it("should correctly attempt to remove all editors", () => {
+        const editors = ["jeanne@darc.fr", "jacques@darc.fr"];
+        editors.forEach((editor) => protection.addEditor(editor));
+
+        protection.removeEditors();
+
+        expect(bareProtection.getEditors()).to.be.empty;
+    });
+});
