f147c2b2c0aeca0993ec1aee0a471815be27004c
Oleg Valter <o.a.philippov@gmail.com>
2021-08-20 17:05:26 +0300

    added data validation test placeholders

---
+++ test/validation.spec.ts
@@ -0,0 +1,39 @@
+import {
+    parseJsonConfigFileContent,
+    readConfigFile,
+    sys,
+    transpile,
+} from "typescript";
+import { runInThisContext } from "vm";
+
+const configContent = readConfigFile("./tsconfig.json", sys.readFile);
+const tsconfig = parseJsonConfigFileContent(configContent.config, sys, "./");
+
+const protectionContent = sys.readFile("./src/validation.ts")!;
+const protection = transpile(protectionContent, tsconfig.options);
+
+runInThisContext(
+    protection + "\n\nObject.assign(this, { DataValidationProtection });"
+);
+
+describe("DataValidationProtection", () => {
+    it.skip("should list protections correctly", () => {});
+
+    it.skip("should get protections by column correctly", () => {});
+
+    it.skip("should get protections by row correctly", () => {});
+
+    it.skip("should protect validations correctly", () => {});
+
+    it.skip("should unprotect validations correctly", () => {});
+
+    it.skip("should add editors to column correctly", () => {});
+
+    it.skip("should remove editors from column correctly", () => {});
+
+    it.skip("should add editors to row correctly", () => {});
+
+    it.skip("should remove editors from row correctly", () => {});
+
+    it.skip("should remove all editors from protections correctly", () => {});
+});
