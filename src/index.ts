38d00a8e3233390d3f77ecc02151ae1595f6f2bc
Oleg Valter <o.a.philippov@gmail.com>
2021-09-03 00:45:49 +0300

    exposed Protection wrapper via wrap method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 Object.assign(this, {
-    getInstance(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    /**
+     * @summary instantiates a DataValidationProtection class
+     * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet target sheet
+     * @returns {DataValidationProtection}
+     */
+    getInstance(
+        sheet: GoogleAppsScript.Spreadsheet.Sheet
+    ): DataValidationProtection {
         return new DataValidationProtection(sheet);
     },
-    setPrefix(prefix: string) {
+
+    /**
+     * @summary sets prefix to add to protection name
+     * @param {string} prefix prefix to set
+     * @returns {void}
+     */
+    setPrefix(prefix: string): void {
         DataValidationProtection.prefix = prefix;
+    },
+
+    /**
+     * @summary wraps a protection into Protection class instance
+     * @param {GoogleAppsScript.Spreadsheet.Protection} protection protection to wrap
+     * @returns {Protection}
+     */
+    wrap(protection: GoogleAppsScript.Spreadsheet.Protection): Protection {
+        return new Protection(protection);
     },
 });
